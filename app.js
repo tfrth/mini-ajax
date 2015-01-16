e31b10c3e0f252d7f4ae45dd7b601bf8bddb79a5
Ty Frith <tyfrith@gmail.com>
2015-01-16 14:14:58 -0700

    mini-ajax complete

--- app.js
+++ app.js
@@ -4,17 +4,6 @@ $(document).ready(function(){
 var arr = [];
 console.log('test');
 
-$('#getUsers').on('click', function() {
-	return $.ajax({
-		method: 'GET',
-		url: 'http://reqr.es/api/users?page=1',
-		success: function(res) {
-		arr = res.data
-		insertData();
-		}
-	})
-})
-
 var insertData = function() {
 	for (var i = 0; i < arr.length; i++) {
 		$('#userInfo' + (i + 1)).html('<div>' +
@@ -31,6 +20,45 @@ var insertData = function() {
 	};
 }
 
+$('#getUsers').on('click', function() {
+	return $.ajax({
+		method: 'GET',
+		url: 'http://reqr.es/api/users?page=1',
+		success: function(res) { //res is the data that is sent back. can actually
+		arr = res.data            // be called "data" . res stands for "response"
+		insertData(res.data);
+		}
+	})
+})
+
+
+$('#addUser').on('click', function(e){
+	e.preventDefault();
+	var userName = $('#name').val();
+	var userJob = $('#job').val();
+	return $.ajax({
+		method: 'POST',
+		url: 'http://reqr.es/api/users',
+		data: {name: userName, job: userJob},  //data we are sending. an object with key of name and key of job
+		success: function(res) {               //getting the data back from API (confirms that API recieved data)
+			$('#recentUser').html(             //html function to add html to recentUSer in DOM
+				'<li>' +
+					'name: ' + res.name +
+				'</li>' +
+				'<li>' +
+					'job: ' + res.job +
+				'</li>' +
+				'<li>' +
+					'id: ' + res.id +
+				'</li>'
+						+
+				'<li>' +
+					'created at: ' + res.createdAt +
+				'</li>'
+			)
+		}
+	})
+});
 
 
 
